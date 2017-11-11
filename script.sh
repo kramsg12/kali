4adab27be6122263498a3f2203af6097bae187f2
kramsg12 <33532860+kramsg12@users.noreply.github.com>
2017-11-11 18:24:50 +1100

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,3 +4,6 @@ tar -xvf tor-browser-linux64-7.0.9_en-US.tar.xz
 useradd toruser
 chown -R toruser tor-browser_en-US # Replace with your TBB folder
 gksu -u toruser tor-browser_en-US/Browser/start-tor-browser
+
+HiddenServiceDir /Library/Tor/var/lib/tor/hidden_service/
+HiddenServicePort 80 127.0.0.1:8085
