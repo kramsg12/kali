45e200d260aa55329cd77df7da3e82df63a14aec
kramsg12 <33532860+kramsg12@users.noreply.github.com>
2017-11-11 18:16:26 +1100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 apt-get install tor
-https://www.torproject.org/dist/torbrowser/7.0.9/tor-browser-linux64-7.0.9_en-US.tar.xz
+wget https://dist.torproject.org/torbrowser/7.0.9/tor-browser-linux64-7.0.9_en-US.tar.xz
 tar -xvf tor-browser-linux64-7.0.9_en-US.tar.xz
