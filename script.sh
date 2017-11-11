23067ce3a4195fe8e24a55da1870b4c64186d14d
kramsg12 <33532860+kramsg12@users.noreply.github.com>
2017-11-11 18:19:23 +1100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,6 @@
 apt-get install tor
 wget https://dist.torproject.org/torbrowser/7.0.9/tor-browser-linux64-7.0.9_en-US.tar.xz
 tar -xvf tor-browser-linux64-7.0.9_en-US.tar.xz
+useradd toruser
+chown -R toruser tor-browser_en-US # Replace with your TBB folder
+gksu -u toruser tor-browser_en-US/Browser/start-tor-browser
