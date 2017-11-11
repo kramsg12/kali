7ebab0346f4f49956817833dd8721efe344a6407
kramsg12 <33532860+kramsg12@users.noreply.github.com>
2017-11-11 18:09:02 +1100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
+sudo dnf install git tor python3-dbus python3-requests python3-beautifulsoup4 python3-gnupg
 cd && git clone https://github.com/yucefsourani/linux-tor-browser-installer
 chmod 755 ~/linux-tor-browser-installer/linux-tor-browser-installer.py
 ~/linux-tor-browser-installer/linux-tor-browser-installer.py
