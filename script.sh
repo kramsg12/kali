ef3ae403056dcbb6fd4d5605014871c6adc6bc66
kramsg12 <33532860+kramsg12@users.noreply.github.com>
2017-11-11 17:42:52 +1100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,6 @@
-
+echo "deb http://deb.i2p2.no/ jessie main"
+wget https://geti2p.net/_static/i2p-debian-repo.key.asc -O /tmp/i2p-debian-repo.key.asc
+apt-key add /tmp/i2p-debian-repo.key.asc
+rm /tmp/i2p-debian-repo.key.asc
+apt-get update
+apt-get install i2p-keyring
